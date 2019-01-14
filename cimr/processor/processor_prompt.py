3b2ecb44d31c3bb67898c17754cb7f80e18c5d76
YoSon Park <yoson.park@gmail.com>
2019-01-14 18:13:27 -0500

    Revert "restructuring processor and adding in base for network"
    This reverts commit 8777e650a32de9a67b221ef40503cb3f554e3840.

--- cimr/processor/processor_prompt.py
+++ cimr/processor/processor_prompt.py
@@ -1,24 +1,33 @@
 #!/usr/bin/env python3
 
 import logging
-from .util import Infiler
 
-
-def process_cli(args):
+def processor_cli(args):
+    from cimr.processor.util import readfile
+    from cimr.processor.util import writefile
+    from cimr.processor.eqtl import checkgene
+    from cimr.processor.tad import annotatetad
+    
     outdir = args.outdir 
     outdir.mkdir(exist_ok=True)
-    from .eqtl import checkgene
-    from .tad import annotatetad
+    logging.info(f' directory {str(outdir)} will be used for cimr jobs.')
+
+    outfile = str(outdir) + '/' + str(args.out) + '_'
+    logging.info(f' file prefix {str(args.out)} will be used for output files')
+    
     """input files are checked for both type-dependant conditions and common formats"""
-    if args.eqtl is not None:
-        infile = Infiler('eqtl', args.eqtl)
-        infile.readfile()
-        checkgene()
-    elif args.gwas is not None:
-        infile = Infiler('gwas', args.gwas)
-        infile.readfile()
-    elif args.tad is not None:
+    if args.eqtlfile is not None:
+        outfile = outfile + 'eqtl.txt'
+        summary_data = readfile(args.eqtl)
+        summary_data = checkgene(summary_data)
+        writefile(summary_data, outfile)
+    elif args.gwasfile is not None:
+        outfile = outfile + 'gwas.txt'
+        summary_data = readfile(args.gwasfile)
+        writefile(summary_data, outfile)
+    elif args.tadfile is not None:
+        outfile = outfile + 'tad.txt'
         annotatetad(args.tad)
     else:
-        logging.info(f'nothing to do')
+        logging.info(f' nothing to do')
 

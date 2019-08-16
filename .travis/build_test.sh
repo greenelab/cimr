de4d901ff35a0f3f0e3fd49d7f57d83b649deaad
YoSon Park <yoson.park@gmail.com>
2019-08-16 13:36:01 -0400

    changed dir name [skip ci]

--- .travis/build_test.sh
+++ .travis/build_test.sh
@@ -2,8 +2,8 @@
 
 set -e -x
 
-cimr processor -process -yaml-file example/submitted/gwas.yml -outdir test
-cimr processor -process -yaml-file example/submitted/eqtl.yml -outdir test
+cimr processor -process -yaml-file examples/expected_submitted/gwas.yml -outdir test
+cimr processor -process -yaml-file examples/expected_submitted/eqtl.yml -outdir test
 
 rm -rf test
 

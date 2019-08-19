6d493f13af76b2b3577c0846944cfa5977d4ca19
YoSon Park <yoson.park@gmail.com>
2019-08-19 11:31:34 -0400

    edit build_test [skip ci]

--- .travis/build_test.sh
+++ .travis/build_test.sh
@@ -2,8 +2,33 @@
 
 set -e -x
 
-cimr processor -process -yaml-file examples/expected_submitted/gwas.yml -outdir test
-cimr processor -process -yaml-file examples/expected_submitted/eqtl.yml -outdir test
+function check_hash {
+
+    data_type=$1
+
+    exdir="examples/expected_processed_data/"${data_type}
+
+    echo "cimr test with data_type ${data_type} example data..."
+
+    cimr processor -process -yaml-file examples/submitted/${data_type}.yml -outdir test
+
+    gzip -dc ${exdir}/${data_type}.txt.gz > ${exdir}/${data_type}.tsv
+    expected_hash=$(md5sum ${exdir}/${data_type}.tsv | awk '{print $1}')
+    gzip -dc test/${data_type}.txt.gz > test/${data_type}.tsv
+    processed_hash=$(md5sum test/${data_type}.tsv | awk '{print $1}')
+
+    if [ "$expected_hash" == "$processed_hash" ]; then
+        echo "build test passed."
+        rm -rf ${exdir}/${data_type}.tsv
+    else
+        exit
+    fi
+
+}
+
+check_hash gwas
+
+check_hash eqtl
 
 rm -rf test
 

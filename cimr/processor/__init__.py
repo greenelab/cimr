f12b294f5e2b5031b3b8be697b6390825ec74742
ypar <yoson.park@gmail.com>
2019-08-29 09:32:22 -0400

    split utils to utils and process

--- cimr/processor/__init__.py
+++ cimr/processor/__init__.py
@@ -1,6 +1,6 @@
 
-from .utils import Infiler
-from .utils import Integrator
+from .process import Infiler
+from .process import Integrator
 from .query import Querier
 from .tad import Tadpole
 from .yamler import Yamler

5a27bccfa277004de3f9d9b1fc46a14c5b8168be
YoSon Park <yoson.park@gmail.com>
2019-08-30 17:01:44 -0400

    restructure examples

--- .circleci/test_data_type.sh
+++ .circleci/test_data_type.sh
@@ -2,22 +2,22 @@
 
 set -e -x
 
-cd examples/
+cd examples/eqtl_gwas/
 
 # Process yaml examples
 for DATATYPE in "eqtl" "gwas"; do
-    cimr processor -process -yaml-file eqtl_gwas/${DATATYPE}.yml
+    cimr processor -process -yaml-file ${DATATYPE}.yml
 
     OUTPUT_PREFIX=${DATATYPE}/${DATATYPE}
     gunzip -k processed_data/${OUTPUT_PREFIX}.txt.gz
 
     # Verify output result
-    md5sum --check eqtl_gwas/md5_${DATATYPE}.txt
+    md5sum --check md5_${DATATYPE}.txt
 
     # Delete "submitted_data" and "processed_data" directories
     rm -rf submitted_data processed_data
 done
 
 # Verify catalog file
-diff catalog.txt eqtl_gwas/expected_catalog.txt
+diff catalog.txt expected_catalog.txt
 rm -f catalog.txt

ccfb5fceb9aa0ef583986883f77c1f881049a288
YoSon Park <yoson.park@gmail.com>
2019-10-11 13:24:38 -0400

    edit dir path

--- .circleci/test_tgz.sh
+++ .circleci/test_tgz.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cd ~/cimr/examples/tgz/
+cd examples/tgz/
 cimr processor -process -yaml-file tgz_gwas.yml
 
 # Decompress the output files ("-k" option keeps the original gz file).
@@ -15,4 +15,8 @@ gunzip -k processed_data/gwas/*.gz
 # (To disable the output message, use "--status" option.)
 # If this command's return status is not zero, CircleCI will quit immediately.
 md5sum --check md5_tgz_gwas.txt
+
+# Check catalog.txt
+diff catalog.txt expected_catalog.txt
+rm -f catalog.txt
 

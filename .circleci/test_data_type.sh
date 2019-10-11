0277e1af6aad997c9bf155aceb84bb8cade79f54
YoSon Park <yoson.park@gmail.com>
2019-10-11 13:15:56 -0400

    updating test data type script

--- .circleci/test_data_type.sh
+++ .circleci/test_data_type.sh
@@ -2,10 +2,14 @@
 
 set -e -x
 
-cd examples/eqtl_gwas/
+cd examples/
 
 # Process yaml examples
 for DATATYPE in "eqtl" "gwas"; do
+    # Get in the specific dir
+    cd ${DATATYPE}/
+
+    # Process example yaml file
     cimr processor -process -yaml-file ${DATATYPE}.yml
 
     OUTPUT_PREFIX=${DATATYPE}/${DATATYPE}
@@ -16,8 +20,13 @@ for DATATYPE in "eqtl" "gwas"; do
 
     # Delete "submitted_data" and "processed_data" directories
     rm -rf submitted_data processed_data
+
+    # Verify catalog file
+    diff catalog.txt expected_catalog.txt
+    rm -f catalog.txt
+
+    # Go back to examples dir
+    cd ..
 done
 
-# Verify catalog file
-diff catalog.txt expected_catalog.txt
-rm -f catalog.txt
+

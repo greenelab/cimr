79204693ed0837b3eb9503ec5eee893e70577ab5
YoSon Park <yoson.park@gmail.com>
2019-11-20 17:13:28 -0500

    additional data checks [skip ci]

--- cimr/processor/utils.py
+++ cimr/processor/utils.py
@@ -20,21 +20,24 @@ from ..defaults import MAXCHROM
 
 
 def set_chrom_dict():
-    """Make a dictionary to standardize chromosome IDs in input files."""
+    """Make a dictionary to standardize chromosome IDs.
+    For sex chromosomes and mitochondria, chr + 'representative letter'
+    format is used for better interpretability.
+    """
     chrom_dict = {
         str(i):'chr' + str(i) for i in range(1, MAXCHROM)
     }
     chrom_dict.update({
-        'X':'chr23',
-        'Y':'chr24',
-        'XY':'chr25',
-        'M':'chr26',
-        'MT':'chr26',
-        'chrX':'chr23',
-        'chrY':'chr24',
-        'chrXY':'chr25',
-        'chrM':'chr26',
-        'chrMT':'chr26'
+        'X':'chrX',
+        'Y':'chrY',
+        'XY':'chrXY',
+        'M':'chrM',
+        'MT':'chrM',
+        'chr23':'chrX',
+        'chr24':'chrY',
+        'chr25':'chrXY',
+        'chr26':'chrM',
+        'chr26':'chrM'
     })
     return chrom_dict, MAXCHROM
 
@@ -89,4 +92,36 @@ def intersect_set(list1, list2):
 def add_line_in_log():
     """Add an arbitrary divider to the log output for readability."""
     logging.info(' ' + '-' * 60 + '\n')
+
+
+def remove_palindromic(df):
+    """Removing palindromic variants from processed data."""
+    df = df.copy()
+    df['g'] = df['effect_allele'].str.upper() + df['non_effect_allele'].str.upper()
+    df = df.loc[~((df.g == 'AT') | (df.g == 'TA') | (df.g == 'CG') | (df.g == 'GC'))]
+    return df.drop('g', axis=1)
+
+
+def try_convert(value, typing):
+    """Try converting input value to specific variable types,
+    e.g. int, float..
+    """
+    converted = True
+    try:
+        value = typing(value)
+    except:
+        converted = False
+    return converted
+
+
+def inferred(value):
+    """Try converting values to specific variable types,
+    return converted value.
+    """
+    if try_convert(value, int):
+        return int(value)
+    elif try_convert(value, float):
+        return float(value, float)
+    return value
+
 

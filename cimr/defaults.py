3dae04b9058ba4613958ac804a8149819e0095be
YoSon Park <yoson.park@gmail.com>
2019-11-22 15:57:09 -0500

    update defaults [skip ci]

--- cimr/defaults.py
+++ cimr/defaults.py
@@ -1,6 +1,11 @@
 """Set of default values used in cimr"""
 
+from collections import namedtuple
+
+
 ANNOTURL = 'https://raw.githubusercontent.com/greenelab/cimr/master/cimr/data/annotation/'
+# CHAINURL = 'https://raw.githubusercontent.com/greenelab/cimr/master/cimr/data/chain/'
+CHAINURL = '/work/annotation/'
 
 # Current maximum is set with human chromosomes:
 # 1 - 22 autosomal chromosomes
@@ -46,14 +51,23 @@ DATA_TYPES = (
 
 GENOME_BUILDS = ('b37', 'b38')
 
-# temporary local file locations. they will be uploaded to zenodo eventually.
+# temporary local file locations
 SNP125HG17 = '/data/refs/dbsnp/snp125_hg17.txt.gz'
 SNP130HG18 = '/data/refs/dbsnp/snp130_hg18.txt.gz'
 SNP150HG19 = '/data/refs/dbsnp/snp150_hg19.txt.gz'
 SNP150HG38 = '/data/refs/dbsnp/snp150_hg38.txt.gz'
 
-HG19TO38 = '/work/annotation/hg19ToHg38.over.chain.gz'
+HG19TO38 = CHAINURL + 'hg19ToHg38.over.chain.gz'
+HG18TO38 = CHAINURL + 'hg18ToHg38.over.chain.gz'
 
+DBSNP_HEADER = namedtuple('DBSNP',
+  ['binary_index', 'chromosome', 'start', 'end', 'name', 'score',
+  'strand', 'ref_ncbi', 'ref_ucsc', 'observed', 'mol_type',
+  'variant_class', 'valid', 'average_heterozigosity', 'average_heterozigosity_se',
+  'functional_category', 'location_type', 'qaulity_weight', 'exceptions',
+  'submitter_count', 'submitters', 'alleles_with_freq', 'alleles',
+  'allele_chromosome_count', 'allele_frequencies', 'bitfields']
+)
 
 WORKING_HEADER = {
     'rsnum', 'variant_id', 'pvalue',
